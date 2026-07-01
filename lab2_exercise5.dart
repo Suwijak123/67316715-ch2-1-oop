3cdaf8fa102074f48beb1d6478795b245816a5a3
Suwijak123 <1suwijakt@gmail.com>
2026-07-01 11:12:22 +0700

    1

--- lab2_exercise5.dart
+++ lab2_exercise5.dart
@@ -3,5 +3,13 @@ void printGrade(int score){
 
   if(score >= 80){
     print('Grade: A');
-  }elseif
+  }else if(score >= 70){
+    print('Grade: B');
+  }else if(score >= 60){
+    print('Grade: C');
+  }else if(score >= 50){
+    print('Grade: D');
+  }else{
+    print('Grade: F');
+  }
 }
